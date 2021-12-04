8c8117bee5af5e97817ebb92890888ebc02495ae
CHRIS_LEE <chrismlee87@gmail.com>
2021-12-03 16:33:17 -0800

    Completed encoding and decoding with samples

--- main.py
+++ main.py
@@ -46,3 +46,46 @@ def decode_image(path_to_png):
 
 
 decode_image("./static/encoded_sample.png")
+
+
+def encode_image(path_to_png, secret_message):
+    """
+    TODO: Add docstring and complete implementation.
+    """
+
+    base_image = Image.open(path_to_png)
+    x_size, y_size = base_image.size
+
+    text_img = Image.new(mode='RGB', size=(x_size, y_size), color=(0, 0, 0))
+    draw = ImageDraw.Draw(text_img)
+
+    relativeness = 50
+    font = ImageFont.truetype("Arial Narrow.ttf", y_size // relativeness)
+    draw.text((x_size // relativeness, y_size // relativeness),
+              secret_message, font=font)
+
+    text_img.save("text_only.png")
+
+    for x in range(x_size):
+        for y in range(y_size):
+
+            raw_rgb = base_image.getpixel((x, y))
+            red = raw_rgb[0]
+            binary = bin(red)
+            binary_list = list(binary)
+            binary_len = len(binary_list)
+
+            if text_img.getpixel((x, y)) == (255, 255, 255):
+                binary_list[binary_len - 1] = "0"
+            else:
+                binary_list[binary_len - 1] = "1"
+
+            new_red = int("".join(binary_list), 2)
+            new_rgb = (new_red, raw_rgb[1], raw_rgb[2])
+            base_image.putpixel((x, y), new_rgb)
+
+    base_image.save("encoded.png", "png")
+
+
+encode_image("./static/164-1645094_film-ratatouille-ratatouille-remy.png",
+             "Apparently reading documentation, not my intuitive genius is the only way to solve this insanely hard problem")
